740bc7de55b30181ccbad520a83cb7b6f2934fe7
Julia Mukhametova <j.mukhametova@itspecial.net>
2021-11-12 17:52:10 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="nav">
+    <div class="container">
+      <nuxt-link :class="{ active: $route.name === 'Home' }" to="/"
+        >Home</nuxt-link
+      >
+      <nuxt-link :class="{ active: $route.name === 'Cart' }" to="/cart">
+        <img src="../assets/cart.svg" class="cart-icon" />
+        <span>{{ product_total }}</span>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.commit("updateCartFromLocalStorage");
+  },
+  computed: {
+    product_total() {
+      return this.$store.state.cart.reduce((sum, current) => {
+        return sum + current.quantity;
+      }, 0);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: rgb(245, 245, 245);
+}
+
+#nav {
+  padding: 10px;
+  width: 100%;
+  height: 30px;
+  background-color: #fff;
+  line-height: 30px;
+
+  a {
+    font-weight: bold;
+    color: darkgray;
+    text-decoration: none;
+    margin: 0 5px;
+    font-size: 1.25rem;
+
+    &.active {
+      color: #2c3e50;
+    }
+  }
+}
+
+.text-center {
+  text-align: center;
+}
+
+.cart-icon {
+  width: 20px;
+  height: 20px;
+}
+
+.container {
+  max-width: 1170px;
+  padding: 0 20px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    hi
+    <ProductDescriptionDrawer
+      :product="product"
+      :active="active.product_drawer"
+      @close-product-drawer="closeProductDrawer"
+    />
+
+    <div class="container">
+      <div class="product-cards-list">
+        <ProductSummaryCard
+          v-for="product in items"
+          :key="product.id"
+          :product="product"
+          @view-product="viewProduct($event)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import items from "@/data/items.js";
+import ProductSummaryCard from "@/components/ProductSummaryCard.vue";
+import ProductDescriptionDrawer from "@/components/ProductDescriptionDrawer.vue";
+
+export default Vue.extend({
+  components: {
+    ProductSummaryCard,
+    ProductDescriptionDrawer,
+  },
+  data() {
+    return {
+      items: items,
+      product: null,
+      active: {
+        product_drawer: false,
+      },
+    };
+  },
+  methods: {
+    viewProduct(product: any) {
+      this.product = product;
+      this.active.product_drawer = true;
+      console.log(this.product);
+    },
+    closeProductDrawer() {
+      this.active.product_drawer = false;
+    },
+  },
+  layout: "default",
+  fetch() {},
+});
+</script>
+
+<style lang="scss">
+.product-cards-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
